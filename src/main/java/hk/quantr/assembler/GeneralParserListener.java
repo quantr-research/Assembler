4cf4d74ce1b6e5a8fdcd6a36e8e3ab05ec7c70ad
Peter <mcheung63@hotmail.com>
2017-09-24 03:28:29 +0800

    walker's listener can have ctx.getText(), but parser's listener has nothing returned by ctx.getText()

--- src/main/java/hk/quantr/assembler/GeneralParserListener.java
+++ src/main/java/hk/quantr/assembler/GeneralParserListener.java
@@ -14,14 +14,20 @@ public class GeneralParserListener implements ParseTreeListener {
 	@Override
 	public void enterEveryRule(ParserRuleContext ctx) {
 		System.out.println(ctx.getText());
+
+//		for (int x = ctx.getStart().getTokenIndex(); x < ctx.getStop().getTokenIndex(); x++) {
+//			System.out.println(ctx.get);
+//		}
 	}
 
 	@Override
 	public void visitTerminal(TerminalNode tn) {
+		System.out.println("tn=" + tn.getText());
 	}
 
 	@Override
 	public void visitErrorNode(ErrorNode en) {
+		System.out.println("en=" + en.getText());
 	}
 
 	@Override
